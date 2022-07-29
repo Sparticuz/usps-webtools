55cffc27dcd3dabedf199767897befd0526fccba
Sparticuz <kyle@kmcnally.net>
2022-07-29 09:29:22 -0400

    Upgrade to @tsconfig and ts-node for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,14 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   extends: ["@sparticuz/eslint-config"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  rules: {
+    "dot-notation": "off",
+  },
   settings: {
     node: {
       allowModules: ["ava"],
